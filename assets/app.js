b1d16b9c0b36390e9d1de75e8b9d0fa75eef25f3
Ashrit Ram Anala <ashritramanala@yahoo.com>
2024-03-27 15:31:19 -0400

    added all untracked files

---
+++ assets/app.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".typing-text", {
+    strings: ["I'm Interested In ","Full Stack", "Data Structures","Algorithms", "Machine Learning", "Data Science", "And...","Tech, In General"],
+    loop: true,
+    typeSpeed: 40,
+    backSpeed: 25,
+    backDelay: 700,
+
+});
