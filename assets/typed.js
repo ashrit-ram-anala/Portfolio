a6d8231d528abfb3d24854ffaa63212408aa80c4
Ashrit Ram Anala <ashritramanala@yahoo.com>
2024-10-19 00:21:21 -0500

    converting to react

--- assets/typed.js
+++
@@ -1,8 +0,0 @@
-var typed = new Typed(".typing-text", {
-    strings: ["I'm Interested In... ", "Full Stack", "Data Structures","Algorithms", "Machine Learning", "Data Science", "And...","Tech, In General", "Scroll For More!"],
-    loop: true,
-    typeSpeed: 40,
-    backSpeed: 25,
-    backDelay: 650,
-    
-});
